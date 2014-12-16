83277fc92e43640b172acbc314c094a8e28f24c0
raagztv <dotnetraghu@gmail.com>
2014-12-16 07:06:04 -0500

    Added test class
    and test for loop

--- src/MvcPaging/TestPager.cs
+++ src/MvcPaging/TestPager.cs
@@ -5,7 +5,15 @@ using System.Text;
 
 namespace MvcPaging
 {
+    
     class TestPager
     {
+        public TestPager()
+        {
+            for (int i = 0; i < 100; i++)
+            {
+                
+            }
+        }
     }
 }
